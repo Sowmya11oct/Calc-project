47d5e8342157d0081b7ff0417b093402ab1aa2c7
Sowmya Balachandran <Sowmya Balachandran>
2017-04-29 13:44:47 -0700

    Initial Commit

---
+++ src/main/java/BasicApp.java
@@ -0,0 +1,31 @@
+/**
+ * File Name: BasicApp.java<br>
+ * Balachandran, Sowmya<br>
+ * Java Boot Camp Exercise<br>
+ * Instructor: Jean-francois Nepton<br>
+ * Created: Apr 29, 2017
+ */
+/**
+ * BasicApp //ADDD (description of class)
+ * <p>
+ * //ADDD (description of core fields)
+ * <p>
+ * //ADDD (description of core methods)
+ * 
+ * @author Balachandran, Sowmya
+ * @version 1.0.0
+ * @since 1.0
+ */
+public class BasicApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("(-_-)");
+		System.out.print("(-_-)");
+		System.out.print("(-_-)");
+		System.out.print("(-_-)");
+	}
+}
